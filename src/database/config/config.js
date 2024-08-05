c04ffd9ce5c78f53faff3ecce73f4229a2a8919d
rodristoch <stoke870@gmail.com>
2024-08-05 10:30:34 -0300

    config.js

---
+++ src/database/config/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "development": {
+    "username": "uhkmtzipchxmu85k",
+    "password": "hxq5EhOxtVCIzVpjWdxW",
+    "database": "b2jz7bbxzhcmo0r2cwkd",
+    "host": "b2jz7bbxzhcmo0r2cwkd-mysql.services.clever-cloud.com",
+    "dialect": "mysql",
+    "define": {
+      "timestamps": false
+    }
+  },
+  "test": {
+    "username": "uhkmtzipchxmu85k",
+    "password": "hxq5EhOxtVCIzVpjWdxW",
+    "database": "database_test",
+    "host": "b2jz7bbxzhcmo0r2cwkd-mysql.services.clever-cloud.com",
+    "dialect": "mysql",
+    "define": {
+      "timestamps": false
+    }
+  },
+  "production": {
+    "username": "uhkmtzipchxmu85k",
+    "password": "hxq5EhOxtVCIzVpjWdxW",
+    "database": "database_production",
+    "host": "b2jz7bbxzhcmo0r2cwkd-mysql.services.clever-cloud.com",
+    "dialect": "mysql",
+    "define": {
+      "timestamps": false
+    }
+  }
+}
